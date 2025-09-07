5e6c27e8028dfafc09cf5ce23ea4da7d6295faa9
hexaltech <contact@hexaltech.fr>
2025-09-07 22:32:29 +0200

    Update map_drive.bat

--- Ansible_WindowsServer/02_Partage_DATAS/files/map_drive.bat
+++ Ansible_WindowsServer/02_Partage_DATAS/files/map_drive.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM Map le lecteur Z: sur le partage FS1\Partage_DATAS avec les identifiants fournis
-net use X: \\FS1\Partage_DATAS /user:HEXALTECH\Administrateur 85432196Alex* /persistent:yes
+net use X: \\FS1\Partage_DATAS /user:HEXALTECH\Administrateur votre_mot_de_passe /persistent:yes
