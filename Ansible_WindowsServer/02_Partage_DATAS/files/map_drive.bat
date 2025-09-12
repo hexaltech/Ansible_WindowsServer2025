0e256d1f35f497f6378e68ba8402613fdab730de
hexaltech <contact@hexaltech.fr>
2025-09-12 13:57:04 +0200

    Delete Ansible_WindowsServer/02_Partage_DATAS/files/map_drive.bat

--- Ansible_WindowsServer/02_Partage_DATAS/files/map_drive.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-REM Map le lecteur Z: sur le partage FS1\Partage_DATAS avec les identifiants fournis
-net use X: \\FS1\Partage_DATAS /user:HEXALTECH\Administrateur 85432196Alex* /persistent:yes
